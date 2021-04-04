2bf6f520e8ab9ddea38570c6b6b38049ea7651c2
anthony8912310 <73423154+anthony8912310@users.noreply.github.com>
2021-04-04 22:06:24 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  sumTest
+//
+//  Created by admin on 2021/4/4.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int m,n;
+    scanf("%d %d",&m,&n);
+    long long product,sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        product = 1;
+        for (int j=1; j<=m; j++) {
+            product*=i;
+            printf("%llu\n",product);
+        }
+        sum += product;
+    }
+    printf("%llu\n",sum);
+    return 0;
+}
